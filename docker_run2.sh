dc4ccaf6f594342b125223fd7972f8887a3fca29
Joel Jhonny Porras Caso <joel@landgorilla.com>
2019-07-06 13:38:36 -0500

    Set 2 error

--- docker_run2.sh
+++ docker_run2.sh
@@ -3,3 +3,4 @@
 docker runner node:7-alpine node --version
 docker run node:6-alpine node --version
 docker run -v $HOME/.m2:/root/.m2 -v $WORKSPACE:/apps/ maven:3-alpine ls -la /apps
+dockerrrrrrrrrrrrr
